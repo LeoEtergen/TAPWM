954006d912ce20bc9dc8a9e9e7fb0a3574aec78a
Leonardo Negrete <leohgn10@gmail.com>
2024-08-27 16:18:25 -0300

    atividade 13

---
+++ atividade13/Exercicio.js
@@ -0,0 +1,27 @@
+// AQUI USEI NOTAÇÃO DE OBJETO LITERAL {}
+var aluno1 = {};
+aluno1.ra = prompt("Insira o RA do aluno 1 (Notação literal):");
+aluno1.nome = prompt("Insira o nome do aluno 1 (Notação literal):");
+alert(`Forma literal: ra=${aluno1.ra}, nome=${aluno1.nome}`);
+
+// FUNÇÃO CONSTRUTORA
+function Aluno(ra, nome) {
+    this.ra = ra;
+    this.nome = nome;
+}
+var raAluno2 = prompt("Insira o RA do aluno 2 (Função Construtora):");
+var nomeAluno2 = prompt("Insira o nome do aluno 2 (Função Construtora):");
+var aluno2 = new Aluno(raAluno2, nomeAluno2);
+alert(`Função Construtora: ra=${aluno2.ra}, nome=${aluno2.nome}`);
+
+// 3. AQUI USEI CLASSE (essa foi a partir do ES6, se não me engano)
+class Aluno1 {
+    constructor(ra, nome) {
+        this.ra = ra;
+        this.nome = nome;
+    }
+}
+var raAluno3 = prompt("Insira o RA do aluno 3 (Classe ES6):");
+var nomeAluno3 = prompt("Insira o nome do aluno 3 (Classe ES6):");
+var aluno3 = new Aluno1(raAluno3, nomeAluno3);
+alert(`Classe (ES6): ra=${aluno3.ra}, nome=${aluno3.nome}`);
